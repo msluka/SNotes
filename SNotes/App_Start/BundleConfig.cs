f58d8dea1217f7b91a51026f04a8e579449b9e60
Lenovo Yoga 2 11 <Lenovo Yoga 2 11@LENOVO-PC>
2017-06-23 23:12:02 +0200

    add jquery plugin autosize textarea; fix notelist sizes and icons.

--- SNotes/App_Start/BundleConfig.cs
+++ SNotes/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace SNotes
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/MyScripts/myScript.js"));
+                        "~/Scripts/MyScripts/myScript.js",
+                        "~/Scripts/jquery.autosize.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- SNotes/Scripts/MyScripts/myScript.js
+++ SNotes/Scripts/MyScripts/myScript.js
@@ -1,15 +1,23 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
 
 
+    //window.setInterval(function(){
+    //    $(function () {
+
+    //        $(".titleAndnoteAdd").remove();
+    //}, 100);
+
+
+    //$("grammarly-btn").remove();
     //$("input[type='text'], textarea").attr('spellcheck', false);
     $(".editableDivs").attr('spellcheck', false);
 
-    $(function() {
+    $(function () {
 
-        $('li').each(function() {
+        $("li").each(function () {
 
             var noteBoxHeight = $(".noteBox", this).outerHeight();
-            var titleHeight = $(".noteTitle", this).outerHeight();
+            //var titleHeight = $(".noteTitle", this).outerHeight();
             var noteLabelHeight = $(".noteLabel", this).outerHeight();
             var noteFooterHeight = $(".noteFooter", this).outerHeight();
 
@@ -17,54 +25,38 @@
 
             $(".editableDivs", this).outerHeight(noteBoxHeight - noteLabelHeight - noteFooterHeight);
 
-            var editableDivsHeight = $(".editableDivs", this).outerHeight();
+            //var editableDivsHeight = $(".editableDivs", this).outerHeight();
 
-            $(".noteContent", this).outerHeight(editableDivsHeight - titleHeight);
+            //$(".noteContent", this).outerHeight(editableDivsHeight - titleHeight);
 
         });
     });
 
 
-    //$('#editLabelsToggle').click(function() {
-
-    //    $("#editLabelInPartial, #deleteLabelInPartial").toggleClass("showLabelEditor hideLabelEditor");
-
-    //});
-
-
-    $('#editLabelsToggle').click(function () {
-        if ($('#editLabelInPartial, #deleteLabelInPartial').css('display') == 'none') {
-            $('#editLabelInPartial, #deleteLabelInPartial').css('display', 'block');
-            $('#editLabelsToggleWrapper').css('background', 'red');
-        }
-        else {
-            $('#editLabelInPartial, #deleteLabelInPartial').css('display', 'none');
-            $('#editLabelsToggleWrapper').css('background', 'orange');
-        }
-    });
-
-
-   
-    //window.setInterval(function(){
-    //    $(function () {
-
-    //        $('textarea').each(function () {
-
-    //            if (this.clientHeight < this.scrollHeight) { this.style.height = this.scrollHeight + 'px' };
-
-    //        });
-    //    });
-    //}, 100);
-
-
     $(function () {
-        //'use strict';
-        $("textarea").on('change keyup paste input', function () {
-            $(this).attr("rows", Math.round($(this).val().split("\n").length || 1, $(this).attr("rows") || 1));
-            $(this).css("width", $(this).css("width"));
-        }).trigger('change');
+
+        var noteBoxHeight = $("#noteBoxInCreateOrEditView").outerHeight();
+
+        var noteFooterHeight = $("#noteFooterInCreateOrEditView").outerHeight();
+
+        $("#editableDivsInAddOrEditNoteView").outerHeight(noteBoxHeight - noteFooterHeight);
+
+        
     });
-    
+
+
+
+  
+
+    //$("textarea").autosize({ append: "\n" });
+  
+    $("textarea").autosize();
+
+
+
+
+
+
     //$(function () {
 
     //    $('textarea').each(function () {
@@ -73,59 +65,261 @@
 
     //    });
     //});
-    
 
-    $(".noteContent, .noteTitle").on("click",
-        function () {
 
-            var parent = $(this).closest('.noteBox');
+});
 
-            if ($('#temporaryId').length) {
 
-            } else {
 
-                var clone = parent.clone();
+//$('#editLabelsToggle').click(function() {
 
-                clone.appendTo(".notes").addClass("expand");
-                clone.attr('id', 'temporaryId');
-                clone.find(".editableDivs").addClass(".expandEditableDivs");
+//    $("#editLabelInPartial, #deleteLabelInPartial").toggleClass("showLabelEditor hideLabelEditor");
 
-                var noteBoxHeight = $(".expand").outerHeight();
-                var titleHeight = $(".expand").find(".noteTitle").outerHeight();
-                var noteLabelHeight = $(".expand").find(".noteLabel").outerHeight();
-                var noteFooterHeight = $(".expand").find(".noteFooter").outerHeight();
+//});
 
-                $(".expand").find(".noteLabel").css({ bottom: noteFooterHeight });
 
-                $(".expand").find(".editableDivs").outerHeight(noteBoxHeight - noteLabelHeight - noteFooterHeight);
+$('#editLabelsToggle').click(function () {
+    if ($('#editLabelInPartial, #deleteLabelInPartial').css('display') == 'none') {
+        $('#editLabelInPartial, #deleteLabelInPartial').css('display', 'block');
+        $('#editLabelsToggleWrapper').css('background', 'red');
+    }
+    else {
+        $('#editLabelInPartial, #deleteLabelInPartial').css('display', 'none');
+        $('#editLabelsToggleWrapper').css('background', 'orange');
+    }
+});
 
-                var editableDivsHeight = $(".expand").find(".editableDivs").outerHeight();
-                $(".expand").find(".noteContent").outerHeight(editableDivsHeight - titleHeight);
-      
-            };
-       
 
-            $("body").click(function (e) {
 
-                if ($(e.target).closest("#temporaryId").length) {
-                    return false;
-                }
-                else {
-                    $("#temporaryId, this").remove();
 
-                    $(".expand").removeClass("expand");
-                    $(".expandTextarea").removeClass("expandTextarea");
 
-                    $("#temporaryId").removeAttr("id");
-                }
+//window.setInterval(function(){
+//    $(function () {
 
-            });
+//        $('textarea').each(function () {
 
-            $(".noteBox").click(function (event) {
+           
+//            if (this.clientHeight < this.scrollHeight) { this.style.height = this.scrollHeight + 'px' };
+            
 
-                event.stopPropagation();
+//        });
+//    });
+//}, 100);
 
-            });
+
+
+
+
+
+//$(function () {
+//    'use strict';
+//    $("textarea").on('change keyup paste input', function () {
+//        $(this).attr("rows", Math.round($(this).val().split("\n").length || 1, $(this).attr("rows") || 1));
+//        $(this).css("width", $(this).css("width"));
+//    }).trigger('change');
+//});
+
+
+
+//$('body').on('keyup', 'textarea', function (e) {
+//    var scrollTop = $(document).scrollTop();
+//    var prevHeight = $(this).height();
+//    $(this).css('height', 'auto');
+//    var nextHeight = this.scrollHeight;
+//    $(this).height(nextHeight);
+//    $(document).scrollTop(scrollTop + (nextHeight - prevHeight));
+//});
+//$('textarea').keyup();
+
+
+
+
+
+
+//setInterval(function () {
+//    $(textarea).css('height', $(textarea)[0].scrollHeight + 2 + 'px')
+//}, 100);
+
+//$(function () {
+
+//    $('textarea').each(function () {
+
+//        if (this.clientHeight < this.scrollHeight) { this.style.height = this.scrollHeight + 'px' };
+
+//    });
+//});
+
+
+$(".editableDivs").on("click",
+    function () {
+
+        var parent = $(this).closest('.noteBox');
+
+        if ($('#temporaryId').length) {
+
+        } else {
+
+            var clone = parent.clone();
+
+            clone.appendTo(".notes").addClass("expand");
+            clone.attr('id', 'temporaryId');
+            clone.find(".editableDivs").addClass(".expandEditableDivs");
+
+            var noteBoxHeight = $(".expand").outerHeight();
+            var titleHeight = $(".expand").find(".noteTitle").outerHeight();
+            var noteLabelHeight = $(".expand").find(".noteLabel").outerHeight();
+            var noteFooterHeight = $(".expand").find(".noteFooter").outerHeight();
+
+            $(".expand").find(".noteLabel").css({ bottom: noteFooterHeight });
+
+            $(".expand").find(".editableDivs").outerHeight(noteBoxHeight - noteLabelHeight - noteFooterHeight);
+
+            var editableDivsHeight = $(".expand").find(".editableDivs").outerHeight();
+            $(".expand").find(".noteContent").outerHeight(editableDivsHeight - titleHeight);
+
+        };
+
+
+        $("body").click(function (e) {
+
+            if ($(e.target).closest("#temporaryId").length) {
+                return false;
+            }
+            else {
+                $("#temporaryId, this").remove();
+
+                $(".expand").removeClass("expand");
+                $(".expandTextarea").removeClass("expandTextarea");
+
+                $("#temporaryId").removeAttr("id");
+            }
 
         });
+
+        $(".noteBox").click(function (event) {
+
+            event.stopPropagation();
+
+        });
+
+        
+
+    });
+
+$('.info').each(function () {
+
+    $(this).hover(function () {
+            $(this).closest(".footerIcons").siblings(".time").css('display', 'block');
+        },
+
+        function () {
+            // on mouseout, reset 
+            $(this).closest(".footerIcons").siblings(".time").css('display', 'none');
+        });
 });
+
+
+//TextAreaExpanding
+
+        //(function ($) {
+
+        //    var cloneCSSProperties = [
+        //        'lineHeight', 'textDecoration', 'letterSpacing',
+        //        'fontSize', 'fontFamily', 'fontStyle',
+        //        'fontWeight', 'textTransform', 'textAlign',
+        //        'direction', 'wordSpacing', 'fontSizeAdjust',
+        //        'whiteSpace', 'wordWrap'
+        //    ];
+
+        //    var textareaCSS = {
+        //        overflow: "hidden",
+        //        position: "absolute",
+        //        top: "0",
+        //        left: "0",
+        //        height: "100%",
+        //        resize: "none",
+        //        //My
+        //        outline: "none",
+        //        border: "none"
+        //    };
+
+        //    var preCSS = {
+        //        display: "block",
+        //        visibility: "hidden",
+        //        //My
+        //        outline: "none",
+        //        border: "none"
+        //    };
+
+        //    var containerCSS = {
+        //        position: "relative",
+        //        //My
+        //        outline: "none",
+        //        border: "none"
+        //    };
+
+        //    var initializedDocuments = {};
+
+        //    function resize(textarea) {
+        //        $(textarea).parent().find("span").text(textarea.value);
+        //    }
+
+        //    function initialize(document) {
+        //        // Only need to initialize events once per document
+        //        if (!initializedDocuments[document]) {
+        //            initializedDocuments[document] = true;
+
+        //            $(document).delegate(
+        //                ".expandingText textarea",
+        //                "input onpropertychange",
+        //                function () {
+        //                    resize(this);
+        //                }
+        //            );
+        //        }
+        //    }
+
+        //    $.fn.expandingTextarea = function () {
+
+        //        return this.filter("textarea").each(function () {
+
+        //            initialize(this.ownerDocument || document);
+
+        //            var textarea = $(this);
+
+        //            textarea.wrap("<div class='expandingText'></div>");
+        //            textarea.after("<pre class='textareaClone'><span></span><br /></pre>");
+
+        //            var container = textarea.parent().css(containerCSS);
+        //            var pre = container.find("pre").css(preCSS);
+
+        //            textarea.css(textareaCSS);
+
+        //            $.each(cloneCSSProperties, function (i, p) {
+        //                pre.css(p, textarea.css(p));
+        //            });
+
+        //            resize(this);
+        //        });
+        //    };
+
+        //    $.fn.expandingTextarea.initialSelector = "textarea.expanding";
+
+        //    $(function () {
+        //        $($.fn.expandingTextarea.initialSelector).expandingTextarea();
+        //    });
+
+        //})(jQuery);
+
+
+       
+
+
+
+
+
+
+
+
+    
+
