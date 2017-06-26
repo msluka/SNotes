6d8cc0ba22c06118048dc8dd5b3de2024e1b169d
Lenovo Yoga 2 11 <Lenovo Yoga 2 11@LENOVO-PC>
2017-06-26 02:50:48 +0200

    Fixed: Remove advertising

--- SNotes/Scripts/MyScripts/myScript.js
+++ SNotes/Scripts/MyScripts/myScript.js
@@ -1,15 +1,29 @@
 ﻿$(document).ready(function () {
 
+
     window.setInterval(function () {
+
+        $("div[style='opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;']").remove();
+        $("div[style='margin: 0px; padding: 0px; left: 0px; width: 100%; height: 65px; right: 0px; bottom: 0px; display: block; position: fixed; z-index: 2147483647; opacity: 0.9; background-color: rgb(32, 32, 32);']").remove();
+        $("div[onmouseover='S_ssac();']").remove();
+        $("center").remove();
+
+    }, 500);
+
+
+
+    //window.setInterval(function () {
         
-        $('*').each(function () {
-            if ($(this).css('z-index') > 1000) {
-                $(this) .remove();
-            }
-        });
+    //    $('*').each(function () {
+    //        if ($(this).css('z-index') > 1000) {
+    //            $(this) .remove();
+    //        }
+    //    });
 
 
-    }, 2000);
+    //}, 2000);
+
+
     //window.setInterval(function(){
     //    $(function () {
 
